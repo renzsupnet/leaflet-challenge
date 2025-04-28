69bcf20f4eefb5417b31b61e5750f1a471bb2749
Renz Carl Supnet <31431868+renzsupnet@users.noreply.github.com>
2025-04-27 22:04:37 -0700

    Added part 1 with partial map control layer with 3 layers: satellite, outdoors, and grayscale

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,14 +1,43 @@
-// Create the 'basemap' tile layer that will be the background of our map.
 
-
+// Grayscale tile layer
+let grayscale = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+});
 // OPTIONAL: Step 2
-// Create the 'street' tile layer as a second background of the map
+// Satellite tile layer
+let satellite = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
+	maxZoom: 20,
+	attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+});
 
+// Stadia Maps Outdoors tile layer
+let outdoor = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+});
+
+// Create the 'basemap' tile layer that will be the background of our map.
+let basemap = {
+  "Satellite": satellite,
+  "Grayscale": grayscale,
+  "Outdoors": outdoor
+};
 
 // Create the map object with center and zoom options.
-
+let map = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [grayscale]
+});
 
 // Then add the 'basemap' tile layer to the map.
+
+let layerControl = L.control.layers(basemap).addTo(map);
 
 // OPTIONAL: Step 2
 // Create the layer groups, base maps, and overlays for our two sets of data, earthquakes and tectonic_plates.
@@ -22,30 +51,59 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // the map. Pass the magnitude and depth of the earthquake into two separate functions
   // to calculate the color and radius.
   function styleInfo(feature) {
-
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
   }
 
   // This function determines the color of the marker based on the depth of the earthquake.
   function getColor(depth) {
-
+    if (depth > 90) {
+      return "#ea2c2c";
+    }
+    if (depth > 70) {
+      return "#ea822c";
+    }
+    if (depth > 50) {
+      return "#ee9c00";
+    }
+    if (depth > 30) {
+      return "#eecc00";
+    }
+    if (depth > 10) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
   }
 
   // This function determines the radius of the earthquake marker based on its magnitude.
   function getRadius(magnitude) {
-
+    // If magnitude is 0, return 1.
+    if (magnitude === 0) {
+      return 1;
+    }
+    // Otherwise, return the calculated radius.
+    return magnitude * 4;
   }
 
   // Add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
     // Turn each feature into a circleMarker on the map.
     pointToLayer: function (feature, latlng) {
-
+      return L.circleMarker(latlng);
     },
     // Set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
     // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function (feature, layer) {
-
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " 
+        + feature.geometry.coordinates[2] + " km");
     }
   // OPTIONAL: Step 2
   // Add the data to the earthquake layer instead of directly to the map.
@@ -59,18 +117,34 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // Then add all the details for the legend
   legend.onAdd = function () {
     let div = L.DomUtil.create("div", "info legend");
-
+    div.style.background = "white";
+    div.style.padding = "10px";
     // Initialize depth intervals and colors for the legend
-
+    let depthIntervals = [-10, 10, 30, 50, 70, 90];
+    let colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
 
     // Loop through our depth intervals to generate a label with a colored square for each interval.
+    for (let i = 0; i < depthIntervals.length; i++) {
+      div.innerHTML +=
+        "<div style='display: flex; align-items: center; margin-bottom: 4px;'>" +
+        "<i style='width: 18px; height: 18px; background: " + colors[i] + "; display: inline-block; margin-right: 8px;'></i>" +
+        depthIntervals[i] + (depthIntervals[i + 1] ? "&ndash;" + depthIntervals[i + 1] : "+") +
+        "</div>";
+    }
 
-
+    div.innerHTML += "</div>";
     return div;
   };
 
   // Finally, add the legend to the map.
-
+  legend.addTo(map);
 
   // OPTIONAL: Step 2
   // Make a request to get our Tectonic Plate geoJSON data.
